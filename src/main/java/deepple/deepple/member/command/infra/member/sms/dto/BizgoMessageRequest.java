9d4021d3915707a2d12471a30cf6f82c97fd9650
공태현 <dev0taehyeon@gmail.com>
2026-01-11 17:58:20 +0900

    [Refac] Change Requestbody for Bizgo (#402)
    * [Refac] : When Match Request or Response, Don't Check Primary Contatct Type
    
    * [fix] : Change BaseURL
    
    * [Fix] : Using Enum
    
    * [Refac] Change BizgoRequestBody

--- src/main/java/deepple/deepple/member/command/infra/member/sms/dto/BizgoMessageRequest.java
+++ src/main/java/deepple/deepple/member/command/infra/member/sms/dto/BizgoMessageRequest.java
@@ -1,8 +1,37 @@
 package deepple.deepple.member.command.infra.member.sms.dto;
 
-public record BizgoMessageRequest(
-    String from,
-    String to,
-    String text
-) {
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+public class BizgoMessageRequest {
+    List<MessageFlow> messageFlow;
+    List<Destination> destinations;
+    String ref;
+
+    @AllArgsConstructor
+    public class MessageFlow {
+        Sms sms;
+    }
+
+    @AllArgsConstructor
+    public class Sms {
+        String from;
+        String text;
+    }
+
+    @AllArgsConstructor
+    public class Destination {
+        String to;
+    }
+
+    public BizgoMessageRequest(String text, String from, String to) {
+        Destination destination = new Destination(to);
+        Sms sms = new Sms(from, text);
+        MessageFlow messageFlow = new MessageFlow(sms);
+
+        this.messageFlow = List.of(messageFlow);
+        this.destinations = List.of(destination);
+        this.ref = null;
+    }
 }
