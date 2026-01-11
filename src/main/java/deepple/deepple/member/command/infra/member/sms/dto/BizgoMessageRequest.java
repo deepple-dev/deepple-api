7ea2a8d83668371365d8be5131e9df5b877e3582
공태현 <dev0taehyeon@gmail.com>
2026-01-11 21:16:19 +0900

    [Fix] : Use Record for serialization

--- src/main/java/deepple/deepple/member/command/infra/member/sms/dto/BizgoMessageRequest.java
+++ src/main/java/deepple/deepple/member/command/infra/member/sms/dto/BizgoMessageRequest.java
@@ -1,37 +1,22 @@
 package deepple.deepple.member.command.infra.member.sms.dto;
 
-import lombok.AllArgsConstructor;
-
 import java.util.List;
 
-public class BizgoMessageRequest {
-    List<MessageFlow> messageFlow;
-    List<Destination> destinations;
-    String ref;
+public record BizgoMessageRequest(
+    List<MessageFlow> messageFlow,
+    List<Destination> destinations,
+    String ref
+) {
 
-    @AllArgsConstructor
-    public class MessageFlow {
-        Sms sms;
-    }
-
-    @AllArgsConstructor
-    public class Sms {
-        String from;
-        String text;
-    }
-
-    @AllArgsConstructor
-    public class Destination {
-        String to;
-    }
+    public record MessageFlow(Sms sms) {}
+    public record Sms(String from, String text) {}
+    public record Destination(String to) {}
 
     public BizgoMessageRequest(String text, String from, String to) {
-        Destination destination = new Destination(to);
-        Sms sms = new Sms(from, text);
-        MessageFlow messageFlow = new MessageFlow(sms);
-
-        this.messageFlow = List.of(messageFlow);
-        this.destinations = List.of(destination);
-        this.ref = null;
+        this(
+            List.of(new MessageFlow(new Sms(from, text))),
+            List.of(new Destination(to)),
+            null
+        );
     }
 }
