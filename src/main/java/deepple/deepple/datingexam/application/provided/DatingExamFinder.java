f3192f6a2d74ff824facf2be7956aeda2af6e3ae
hainho <hainho9704@gmail.com>
2026-03-05 03:10:42 +0900

    feat: 가장 count 높은 personality type 조회 api 구현

--- src/main/java/deepple/deepple/datingexam/application/provided/DatingExamFinder.java
+++ src/main/java/deepple/deepple/datingexam/application/provided/DatingExamFinder.java
@@ -1,9 +1,12 @@
 package deepple.deepple.datingexam.application.provided;
 
 import deepple.deepple.datingexam.application.dto.DatingExamInfoWithSubjectSubmissionResponse;
+import deepple.deepple.datingexam.application.dto.DominantPersonalityTypeResponse;
 
 public interface DatingExamFinder {
     DatingExamInfoWithSubjectSubmissionResponse findRequiredExamInfo(Long memberId);
 
     DatingExamInfoWithSubjectSubmissionResponse findOptionalExamInfo(Long memberId);
+
+    DominantPersonalityTypeResponse findDominantPersonalityType(Long memberId);
 }
