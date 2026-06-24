9ed5a740457454c1e4ddc2b9493a07da5490f3a3
hoyunjung <jhy9094@gmail.com>
2026-06-24 22:11:17 +0900

    feat(member): 회원 마지막 접속 시각 갱신 메서드에 트랜잭션 추가
    #447

--- src/main/java/deepple/deepple/member/command/infra/member/MemberCommandRepositoryImpl.java
+++ src/main/java/deepple/deepple/member/command/infra/member/MemberCommandRepositoryImpl.java
@@ -6,6 +6,7 @@ import deepple.deepple.member.command.domain.member.vo.KakaoId;
 import deepple.deepple.member.command.domain.member.vo.PhoneNumber;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -64,6 +65,7 @@ public class MemberCommandRepositoryImpl implements MemberCommandRepository {
     }
 
     @Override
+    @Transactional
     public void updateLastAccessedAt(Long memberId, LocalDateTime accessedAt) {
         memberCommandJpaRepository.updateLastAccessedAt(memberId, accessedAt);
     }
